6e3b2b863e92251a0fe2b778a01de72c75016474
gabrielmota7 <motagabriel819@gmail.com>
2021-10-26 22:55:36 -0300

    get

---
+++ calc.js
@@ -0,0 +1,13 @@
+var SomaFunc = require("./somar");
+var MultiFunc = require("./multi");
+var SubFunc = require("./sub")
+var DivFunc = require("./div");
+
+
+
+
+
+console.log(SomaFunc(1,2))
+console.log(MultiFunc(100,10))
+console.log(SubFunc(80,10))
+console.log(DivFunc(100,2))
